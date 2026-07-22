bcebb07946d33bd28fe7353ae470256fde3501d9
gbe-gif <workingsh2716@gmail.com>
2026-07-22 22:05:32 +0900

    2.0.5

--- image_card.py
+++ image_card.py
@@ -1,54 +1,40 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 from PySide6.QtCore import Qt
 
-
 class ImageCard(QWidget):
     def __init__(self, turn, files, pixmap):
         super().__init__()
-
+        
         self.turn = turn
         self.files = files
-        self.original_pixmap = pixmap
-
-        self._current_width = None
-
+        self.original_pixmap = pixmap 
+        
         self._setup_ui()
-
+        
     def _setup_ui(self):
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 20)
         layout.setSpacing(5)
-
+        
+        # v1.2 변경: 멤버 변수로 승격
         self.turn_label = QLabel(f"Turn {self.turn}")
         font = self.turn_label.font()
         font.setBold(True)
         font.setPointSize(11)
         self.turn_label.setFont(font)
-
+        
         self.image_label = QLabel()
+        self.image_label.setPixmap(self.original_pixmap)
         self.image_label.setAlignment(Qt.AlignCenter)
-
+        
         layout.addWidget(self.turn_label)
         layout.addWidget(self.image_label)
 
-        self.image_label.setPixmap(self.original_pixmap)
-
     def set_display_width(self, width):
-        # 같은 크기로 또 스케일링하지 않음
-        if width == self._current_width:
-            return
-
-        self._current_width = width
-
-        # 원본보다 크게 확대하지 않음
-        target_width = min(width, self.original_pixmap.width())
-
-        # FastTransformation이 텍스트를 더 또렷하게 유지하는 경우가 많음
-        scaled = self.original_pixmap.scaled(
-            target_width,
-            self.original_pixmap.height(),
-            Qt.KeepAspectRatio,
-            Qt.FastTransformation
-        )
-
-        self.image_label.setPixmap(scaled)
+        if self.original_pixmap.width() > width:
+            scaled_pixmap = self.original_pixmap.scaledToWidth(
+                width, Qt.SmoothTransformation
+            )
+            self.image_label.setPixmap(scaled_pixmap)
+        else:
+            self.image_label.setPixmap(self.original_pixmap)
